52755cabdd89dfcfe2fe59091a751af6d316d226
hirogata <oh1romichi@gmail.com>
2015-09-10 11:07:30 +0900

    Initial commit

---
+++ lib/itamae/plugin/resource/route.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/route/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Route
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/route/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Route
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
