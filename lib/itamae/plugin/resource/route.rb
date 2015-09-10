aa6f83c09ce4a163f78994ec74693a3c3e1d14b1
hirogata <oh1romichi@gmail.com>
2015-09-10 11:45:43 +0900

    Add routing resource

--- lib/itamae/plugin/resource/route.rb
+++ lib/itamae/plugin/resource/route.rb
@@ -3,8 +3,59 @@ require 'itamae/resource/base'
 module Itamae
   module Plugin
     module Resource
-      module Route
-        # Your code goes here...
+      class Route < Itamae::Resource::Base
+        define_attribute :action, default: :create
+        define_attribute :netmask, type: String
+        define_attribute :gateway, type: String
+        define_attribute :target, type: String
+        define_attribute :device, type: String
+
+        MASK = {'0.0.0.0'          => '0',
+                '128.0.0.0'        => '1',
+                '192.0.0.0'        => '2',
+                '224.0.0.0'        => '3',
+                '240.0.0.0'        => '4',
+                '248.0.0.0'        => '5',
+                '252.0.0.0'        => '6',
+                '254.0.0.0'        => '7',
+                '255.0.0.0'        => '8',
+                '255.128.0.0'      => '9',
+                '255.192.0.0'      => '10',
+                '255.224.0.0'      => '11',
+                '255.240.0.0'      => '12',
+                '255.248.0.0'      => '13',
+                '255.252.0.0'      => '14',
+                '255.254.0.0'      => '15',
+                '255.255.0.0'      => '16',
+                '255.255.128.0'    => '17',
+                '255.255.192.0'    => '18',
+                '255.255.224.0'    => '19',
+                '255.255.240.0'    => '20',
+                '255.255.248.0'    => '21',
+                '255.255.252.0'    => '22',
+                '255.255.254.0'    => '23',
+                '255.255.255.0'    => '24',
+                '255.255.255.128'  => '25',
+                '255.255.255.192'  => '26',
+                '255.255.255.224'  => '27',
+                '255.255.255.240'  => '28',
+                '255.255.255.248'  => '29',
+                '255.255.255.252'  => '30',
+                '255.255.255.254'  => '31',
+                '255.255.255.255'  => '32' }
+
+        def action_create(options)
+          common_route_items = ''
+          common_route_items << "/#{MASK[attributes.netmask]}" if attributes.netmask
+          common_route_items << " via #{attributes.gateway} " if attributes.gateway
+
+          command = "ip route replace #{attributes.target}"
+          command << common_route_items
+          command << " dev #{attributes.device} " if attributes.device
+
+          run_command(command)
+        end
+
       end
     end
   end
